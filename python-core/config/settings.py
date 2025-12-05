695604333ecc16954e4bf9705ec613d8b82ffe4f
Zeynep Sude ÖNCÜ <zeynep64sude@gmail.com>
2025-12-05 12:05:50 +0300

    Duplicate ve db düzeltildi

--- python-core/config/settings.py
+++ python-core/config/settings.py
@@ -2,8 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 class Settings(BaseSettings):
-    #CHROMA_PATH: str = "python-core/scripts_setup/chroma_db"
-    CHROMA_PATH: str = "chroma_db"
+    # Vector DB path (correct)
+    # Artık absolute path olarak veriyoruz
+    CHROMA_PATH: str = str(Path(__file__).resolve().parent.parent / "chroma_db")
+
     EMBEDDING_MODEL: str = "multi-qa-distilbert-cos-v1"
     LLM_MODEL: str = "llama3:8b"
     OLLAMA_HOST: str = "http://localhost:11434"
@@ -12,7 +14,6 @@ class Settings(BaseSettings):
     LANGCHAIN_TRACING_V2: bool = False
     LANGCHAIN_PROJECT: str | None = None
 
-    # pydantic settings
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
@@ -24,6 +25,9 @@ class Settings(BaseSettings):
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR.parent / "data"
 DOCUMENTS_DIR = DATA_DIR / "documents"
-VECTOR_DB_PATH = BASE_DIR / "scripts_setup" / "chroma_db"
+
+CHROMA_PATH = str(BASE_DIR / "chroma_db")
+VECTOR_DB_PATH = BASE_DIR / "chroma_db"
 
 settings = Settings()
+
--- python-core/database/vector_db.py
+++ python-core/database/vector_db.py
@@ -1,22 +1,21 @@
 from langchain_chroma import Chroma
 from models.embedding_model import get_embedding_model
-from config.settings import settings, VECTOR_DB_PATH
+from config.settings import CHROMA_PATH
 import os
 
 embeddings = get_embedding_model()
+
 def get_vector_db():
-    os.makedirs(settings.CHROMA_PATH, exist_ok=True)
+    os.makedirs(CHROMA_PATH, exist_ok=True)
     db = Chroma(
-        collection_name="test_name", # multiple collections within the same database
-        persist_directory=settings.CHROMA_PATH,
+        collection_name="test_name",
+        persist_directory=CHROMA_PATH,
         embedding_function=embeddings,
-        #collection_name = 'RAG',
     )
     return db
 
-
 retriever = Chroma(
-    collection_name='test_name',
+    collection_name="test_name",
     embedding_function=embeddings,
-    persist_directory=VECTOR_DB_PATH
+    persist_directory=CHROMA_PATH
 ).as_retriever()
