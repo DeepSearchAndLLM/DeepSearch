94757883860cf566914bdbc1d5dfd9bc869b2be1
semihcngz <scengiz2002@hotmail.com>
2026-01-12 12:33:01 +0300

    deneme oncesi
    # Conflicts:
    #	python-core/chroma_db/chroma.sqlite3
    #	python-core/chroma_db/eebad40e-85d7-416c-81f1-84114d14f83a/data_level0.bin
    #	python-core/config/settings.py
    #	python-core/graph/nodes/generate.py

--- python-core/graph/state.py
+++ python-core/graph/state.py
@@ -1,6 +1,17 @@
 from typing import List, TypedDict, Any
 from langchain_core.documents import Document
-
+'''
+class GraphState(TypedDict, total=False):
+    question: str
+    documents: List[Document]
+    generation: str
+    #local_search: bool
+    generation_retries: int
+    #local_search_attempted: bool
+    sources: list[str]
+    meta: Any
+    llm_generation: bool
+'''
 class GraphState(TypedDict, total=False):
     question: str
     documents: List[Document]
--- python-core/main.py
+++ python-core/main.py
@@ -1,3 +1,33 @@
+'''
+from graph.graph import build_graph
+
+
+def main():
+    question = input("🔍 Question: ")
+
+    app = build_graph()
+
+
+    result = app.invoke({"question": question})
+
+
+    print("\n--- GENERATED ANSWER ---\n")
+    print(result.get("generation", "No answer."))
+
+
+    sources = result.get("sources", [])
+
+    if sources:
+        print("\n📄 Sources used:")
+        for src in sources:
+            print(f"- {src}")
+    else:
+        print("\n📄 Sources used: None (general knowledge or no relevant documents)")
+
+
+if __name__ == "__main__":
+    main()
+'''
 from graph.graph import build_graph
 
 
